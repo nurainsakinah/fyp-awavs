00f94149b24d2b0d0e40e1007dcd6ff74f01c7d5
nurainsakinah <ainsakinah009@gmail.com>
2024-01-10 07:33:11 +0800

    initial commit

--- application.py
+++ application.py
@@ -250,4 +250,4 @@ def sqli():
 if __name__ == '__main__':
     with application.app_context():
         db.create_all()
-    application.run(host='0.0.0.0', port=5000, debug=False)
+    application.run(debug=True)
